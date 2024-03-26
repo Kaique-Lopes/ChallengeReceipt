ea654af68a3678ecf50108caa8e9eae053362c02
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2024-03-26 16:44:59 -0300

    Initial commit

---
+++ Model/Receipt.swift
@@ -0,0 +1,35 @@
+//
+//  Receipt.swift
+//  ChallengeReceipt
+//
+//  Created by Kaique Lopes de Oliveira on 23/03/24.
+//
+
+import Foundation
+
+struct Receipt: Codable {
+    let title: String
+    let receiptId: String
+    let name: String
+    let receiverName: String
+    let amount: String
+    let control: String
+    let date: String
+}
+
+struct DataObject: Codable {
+    let data: ReceiptData
+}
+
+struct ReceiptData: Codable {
+    let receipts: [Receipt]
+    let pagination: Pagination
+}
+
+struct Pagination: Codable {
+    let page: Int
+    let pageSize: Int
+    let totalElements: Int
+    let totalPages: Int
+}
+
